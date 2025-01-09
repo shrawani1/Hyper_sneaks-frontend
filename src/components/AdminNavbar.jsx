9593b24a8536f1fcae323d7e9bc0e841ff4538e9
Shrawani <Shrawanizzzkc@gmail.com>
2025-01-10 02:26:29 +0545

    admin navbar securew

--- src/components/AdminNavbar.jsx
+++ src/components/AdminNavbar.jsx
@@ -11,7 +11,7 @@ const AdminNavbar = () => {
       // Fetch additional data if needed
       const fetchUserData = async () => {
         try {
-          const response = await fetch(`http://localhost:3000/user/${user._id}`);
+          const response = await fetch(`https://localhost:3000/user/${user._id}`);
           if (!response.ok) throw new Error('Network response was not ok');
           const data = await response.json();
           setWelcomeMessage(`Welcome back, ${data.firstName}!`);
