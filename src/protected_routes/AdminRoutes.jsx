2874c833d7ff94a30a105fdda343780a062225c8
Shrawani <Shrawanizzzkc@gmail.com>
2025-02-02 16:38:22 +0545

    admin routes

--- src/protected_routes/AdminRoutes.jsx
+++ src/protected_routes/AdminRoutes.jsx
@@ -4,10 +4,7 @@ import { Outlet, Navigate } from 'react-router-dom'
 const AdminRoutes = () => {
   // get user information 
   const user = JSON.parse(localStorage.getItem('user'))
-  // check user
-  //check isAdmin= true
-  //if true: Access all the route of Admin(outlet)
-  //if false: Navigate to Login 
+   
   
   return user !=null && user.isAdmin ? <Outlet/> : <Navigate to={'/login'}/>   
 }
